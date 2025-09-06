31eef3c93b566147598308f44d294c2df7e72236
rupamghosh2006 <rupamgh2006@gmail.com>
2025-09-06 18:05:18 +0530

    my chat app is now working fine

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require("http");
 const express = require("express");
 const path = require("path")
+const { Server } = require("socket.io")
 
 const app = express()
 const server = http.createServer(app);
+const io = new Server(server)
+
+// Socket.io
+io.on('connection', (socket) => {
+    socket.on("user-message", (message) => {
+        io.emit("message", message);
+    });
+});
 
 app.use(
     express.static(path.resolve("./public"))
@@ -13,4 +22,6 @@ app.get("/", (req, res) => {
     return res.sendFile("/public/index.html")
 })
 
-server.listen(9000,  () => console.log(`Server Started at port 9000`));
+server.listen(9000,  () => {
+    console.log(`Server Started at port 9000`)
+});
